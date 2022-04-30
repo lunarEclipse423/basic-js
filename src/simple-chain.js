506573c9cd84f35effc9920f9fd5b7d491a616ec
Maria White <redstone.mari@gmail.com>
2022-04-30 14:24:29 +0300

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      !Number.isInteger(position) ||
+      this.chain.length < position ||
+      position < 1
+    ) {
+      this.chain.splice(0, this.chain.length);
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain.length === 0) {
+      return this;
+    }
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    this.chain = this.chain.map((element) => "( " + String(element) + " )");
+    let result = this.chain.join("~~");
+    this.chain.splice(0, this.chain.length);
+    return result;
+  },
 };
 
 module.exports = {
