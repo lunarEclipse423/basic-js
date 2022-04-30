b575792ba87a5609e0354f2472557d4ceef5b158
Maria White <redstone.mari@gmail.com>
2022-04-30 14:27:40 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("'arr' parameter must be an instance of the Array!");
+  }
+
+  let transformedArray = []
+  let wasDiscarded = false;
+  for (let i = 0; i < arr.length; ++i) {
+    if (typeof arr[i] === "string") {
+      switch(arr[i]) {
+        case "--discard-next":
+          if (i + 1 !== arr.length) {
+            ++i;
+            wasDiscarded = true;
+          }
+          break;
+        case "--discard-prev":
+          if (i !== 0 && wasDiscarded !== true) {
+            transformedArray.pop();
+          }
+          wasDiscarded = false;
+          break;
+        case "--double-next":
+          if (i + 1 !== arr.length) {
+            transformedArray.push(arr[i + 1]);
+          }
+          break;
+        case "--double-prev":
+          if (i !== 0 && wasDiscarded !== true) {
+            transformedArray.push(arr[i - 1]);
+          }
+          wasDiscarded = false;
+          break;
+        default:
+          transformedArray.push(arr[i]);
+      }
+    } else {
+      transformedArray.push(arr[i]);
+    }
+  }
+  return transformedArray;
 }
 
 module.exports = {
