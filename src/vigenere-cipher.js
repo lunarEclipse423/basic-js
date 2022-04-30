62ce0f9b2a0957b317012afb56588bca03ce7658
Maria White <redstone.mari@gmail.com>
2022-04-30 14:28:58 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+ const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type || arguments.length === 0 ? "direct" : "reverse";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let bigMessage = message.toUpperCase();
+    let bigKey = key.toUpperCase();
+    let result = "";
+    let currIndex = 0;
+    for (let i = 0; i < bigMessage.length; ++i) {
+      if (ALPHABET.indexOf(bigMessage[i]) === -1) {
+        result += bigMessage[i];
+        continue;
+      }
+      result +=
+        ALPHABET[
+          (ALPHABET.indexOf(bigMessage[i]) +
+            ALPHABET.indexOf(bigKey[currIndex])) %
+            26
+        ];
+      currIndex++;
+      currIndex === bigKey.length ? (currIndex = 0) : currIndex;
+    }
+    return this.type === "direct"
+      ? result
+      : result.split("").reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let bigMessage = encryptedMessage.toUpperCase();
+    let bigKey = key.toUpperCase();
+    let result = "";
+    let currIndex = 0;
+    for (let i = 0; i < bigMessage.length; ++i) {
+      if (ALPHABET.indexOf(bigMessage[i]) === -1) {
+        result += bigMessage[i];
+        continue;
+      }
+      let currLetter =
+        (ALPHABET.indexOf(bigMessage[i]) -
+          ALPHABET.indexOf(bigKey[currIndex])) %
+        26;
+      while (currLetter < 0) {
+        currLetter += 26;
+      }
+      result += ALPHABET[currLetter];
+      currIndex++;
+      currIndex === bigKey.length ? (currIndex = 0) : currIndex;
+    }
+    return this.type === "direct"
+      ? result
+      : result.split("").reverse().join("");
   }
 }
 
