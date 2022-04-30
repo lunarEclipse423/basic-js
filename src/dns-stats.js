59ffd772255896f971e884c9efb727dc42a9cf2d
Maria White <redstone.mari@gmail.com>
2022-04-30 14:12:26 +0300

    feat: solve dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  for (let i = 0; i < domains.length; ++i) {
+    let currDomain = domains[i].split(".");
+    let index = currDomain.length - 1;
+    let acc = "";
+    while (index >= 0) {
+      acc += "." + currDomain[index];
+      obj[acc] = obj[acc] ? (obj[acc] += 1) : 1;
+      index--;
+    }
+  }
+  return obj;
 }
 
 module.exports = {
