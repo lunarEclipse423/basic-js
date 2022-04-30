79ed8f147a4eac3127f501a62c147c55e74778b8
Maria White <redstone.mari@gmail.com>
2022-04-30 14:13:25 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let teamName = "";
+  for (let i = 0; i < members.length; ++i) {
+    if (typeof members[i] === "string") {
+      let temp = members[i].trim();
+      teamName += temp[0].toUpperCase();
+    }
+  }
+  return teamName.split("").sort().join("");
 }
 
 module.exports = {
